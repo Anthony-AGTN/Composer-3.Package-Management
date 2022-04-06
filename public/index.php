9c525d4b1ba99a8436a6c63aafec4e2ece462b19
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-04-06 10:57:22 +0200

    add crazy cow

--- public/index.php
+++ public/index.php
@@ -1,2 +1,21 @@
 <?php
 
+require '../vendor/autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
+
+$bessie = new Cow('Hello, Farm!');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
